5ebe20c6da231cf2f3ef3336a78021caeb912c95
ronnimadeira <115476262+ronnimadeira@users.noreply.github.com>
2023-08-02 17:20:18 -0300

    Atualizar 'pxt.json, main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,35 +1,24 @@
 input.onButtonPressed(Button.A, function () {
-    if (numero > 100) {
-        numero += -100
-    }
     basic.clearScreen()
-    basic.showNumber(numero)
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.clearScreen()
-    basic.showNumber(pins.analogReadPin(AnalogPin.P1))
-    basic.pause(1000)
-    basic.clearScreen()
-    basic.showNumber(numero)
-    basic.pause(1000)
+    basic.showNumber(input.temperature())
+    basic.pause(2000)
 })
 input.onButtonPressed(Button.B, function () {
-    if (numero < 1024) {
-        numero += 100
-    }
     basic.clearScreen()
-    basic.showNumber(numero)
+    basic.showNumber(input.compassHeading())
+    basic.pause(2000)
 })
 let umidade = 0
-let numero = 0
-numero = 900
+let número = 900
 basic.forever(function () {
+    basic.clearScreen()
     umidade = pins.analogReadPin(AnalogPin.P1)
-    if (umidade >= numero) {
-        basic.showIcon(IconNames.Happy)
-        music.playTone(262, music.beat(BeatFraction.Whole))
-    } else {
-        basic.showIcon(IconNames.Sad)
+    led.plotBarGraph(
+    umidade,
+    1023
+    )
+    if (umidade >= número) {
         music.playTone(698, music.beat(BeatFraction.Whole))
     }
+    basic.pause(2000)
 })
